aa275cf511ffab85ec450c5a5105ce4410dc140d
Sithabiseni <sithabisenimtshali@gmail.com>
2024-04-30 19:31:58 +0200

    url

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "https://r5hlyskcd4.execute-api.eu-west-1.amazonaws.com",
+                url: "https://ob9hy2sv0k.execute-api.eu-west-1.amazonaws.com/Sithabiseni_Lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
