93a1546d32c18d774b511213769d8f4901417e04
sithabisenim <111012312+sithabisenim@users.noreply.github.com>
2024-05-02 22:12:20 +0200

    Update contact_me.js

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://mu1t9x3lcg.execute-api.eu-west-1.amazonaws.com/2401FTDE-SITMTS-CC-predict-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
